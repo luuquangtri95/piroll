ae9fa54bb611c9aa20eab3a3875a37ec1b26db63
Luu Quang Tri <luuquangtri95@gmail.com>
2021-08-08 13:23:18 +0700

    add slide

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,7 +23,38 @@ $(document).ready(function () {
     ],
   });
 });
-
+$(document).ready(function () {
+  $('.brand__list').slick({
+    prevArrow:
+      "<button type='button' class='slick-prev pull-left'><i class='fal fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:
+      "<button type='button' class='slick-next pull-right'><i class='fal fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [
+      {
+        breakpoint: 1920,
+        settings: {
+          slidesToShow: 5,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 479,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+          autoplay: true,
+        },
+      },
+    ],
+  });
+});
 // menu-mobile
 const toggleMenu = document.querySelector('.header__toggle-menu');
 const menuList = document.querySelector('.header__menu-list');
